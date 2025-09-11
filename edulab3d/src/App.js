b95f9c8f482b92f2c4945d6a598f2353ed83ac53
HAYULE MIN <mhy9203.develop@gmail.com>
2025-09-11 05:19:55 +0000

    add some React Bits codes

--- edulab3d/src/App.js
+++ edulab3d/src/App.js
@@ -1,21 +1,34 @@
+import React from 'react';
 import logo from './logo.svg';
+import TextType from './TextType';
 import './App.css';
 
 function App() {
+  const messages = [
+    'Hello!', 
+    'Welcome to join our Universe, EduLab3D!', 
+    'You can enjoy science experiences!',
+    'Let`s start Today`s science experiences!',
+    'Made for Jungheung Middle School',
+  ];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <TextType
+          text={messages}
+          typingSpeed={60}
+          pauseDuration={1500}
+          deletingSpeed={30}
+          cursorCharacter="|"
+        />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://edulab3d.tech"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go to Main page
         </a>
       </header>
     </div>
